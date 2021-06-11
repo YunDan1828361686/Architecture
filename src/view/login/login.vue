d04e49e75cf88c3a442205a28ea30bb955914959
YunDan1828361686 <1828361686@qq.com>
2021-06-11 14:49:50 +0800

    终版，添加账号密码的提示

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -76,231 +76,237 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
-import { Spin } from "view-design";
-import $ from "jquery";
+import { mapActions } from 'vuex'
+import { Spin } from 'view-design'
+import $ from 'jquery'
 export default {
-  data() {
+  data () {
     return {
       login_spin: false,
       // 生成验证码的函数
-      verificationCode: "",
+      verificationCode: '',
       // 验证码
-      verVal: "",
-    };
+      verVal: ''
+    }
   },
   components: {},
-  mounted() {
+  mounted () {
+    this.$Message['success']({
+      background: true,
+      content: '账号是super_admin或者admin,密码随便填,验证码随便填！',
+      duration: 5
+    })
     // 验证码
-    var that = this;
+    var that = this
     this.verificationCode = {
-      init() {
+      init () {
         var nums = [
-          "1",
-          "2",
-          "3",
-          "4",
-          "5",
-          "6",
-          "7",
-          "8",
-          "9",
-          "0",
-          "A",
-          "B",
-          "C",
-          "D",
-          "E",
-          "F",
-          "G",
-          "H",
-          "I",
-          "J",
-          "K",
-          "L",
-          "M",
-          "N",
-          "O",
-          "P",
-          "Q",
-          "R",
-          "S",
-          "T",
-          "U",
-          "V",
-          "W",
-          "X",
-          "Y",
-          "Z",
-          "a",
-          "b",
-          "c",
-          "d",
-          "e",
-          "f",
-          "g",
-          "h",
-          "i",
-          "j",
-          "k",
-          "l",
-          "m",
-          "n",
-          "o",
-          "p",
-          "q",
-          "r",
-          "s",
-          "t",
-          "u",
-          "v",
-          "w",
-          "x",
-          "y",
-          "z",
-        ];
-        var str = "";
-        that.verVal = drawCode();
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          '7',
+          '8',
+          '9',
+          '0',
+          'A',
+          'B',
+          'C',
+          'D',
+          'E',
+          'F',
+          'G',
+          'H',
+          'I',
+          'J',
+          'K',
+          'L',
+          'M',
+          'N',
+          'O',
+          'P',
+          'Q',
+          'R',
+          'S',
+          'T',
+          'U',
+          'V',
+          'W',
+          'X',
+          'Y',
+          'Z',
+          'a',
+          'b',
+          'c',
+          'd',
+          'e',
+          'f',
+          'g',
+          'h',
+          'i',
+          'j',
+          'k',
+          'l',
+          'm',
+          'n',
+          'o',
+          'p',
+          'q',
+          'r',
+          's',
+          't',
+          'u',
+          'v',
+          'w',
+          'x',
+          'y',
+          'z'
+        ]
+        var str = ''
+        that.verVal = drawCode()
         // 绘制验证码
-        function drawCode(str) {
-          var canvas = document.getElementById("verifyCanvas"); // 获取HTML端画布
-          var context = canvas.getContext("2d"); // 获取画布2D上下文
-          context.fillStyle = "cornflowerblue"; // 画布填充色
-          context.fillRect(0, 0, canvas.width, canvas.height); // 清空画布
-          context.fillStyle = "white"; // 设置字体颜色
-          context.font = "25px Arial"; // 设置字体
-          var rand = new Array();
-          var x = new Array();
-          var y = new Array();
+        function drawCode (str) {
+          var canvas = document.getElementById('verifyCanvas') // 获取HTML端画布
+          var context = canvas.getContext('2d') // 获取画布2D上下文
+          context.fillStyle = 'cornflowerblue' // 画布填充色
+          context.fillRect(0, 0, canvas.width, canvas.height) // 清空画布
+          context.fillStyle = 'white' // 设置字体颜色
+          context.font = '25px Arial' // 设置字体
+          var rand = new Array()
+          var x = new Array()
+          var y = new Array()
           for (var i = 0; i < 4; i++) {
-            rand.push(rand[i]);
-            rand[i] = nums[Math.floor(Math.random() * nums.length)];
-            x[i] = i * 20 + 10;
-            y[i] = Math.random() * 20 + 20;
-            context.fillText(rand[i], x[i], y[i]);
+            rand.push(rand[i])
+            rand[i] = nums[Math.floor(Math.random() * nums.length)]
+            x[i] = i * 20 + 10
+            y[i] = Math.random() * 20 + 20
+            context.fillText(rand[i], x[i], y[i])
           }
-          str = rand.join("").toUpperCase();
+          str = rand.join('').toUpperCase()
           // 画3条随机线
           for (var i = 0; i < 3; i++) {
-            drawline(canvas, context);
+            drawline(canvas, context)
           }
 
           // 画30个随机点
           for (var i = 0; i < 30; i++) {
-            drawDot(canvas, context);
+            drawDot(canvas, context)
           }
-          convertCanvasToImage(canvas);
-          return str;
+          convertCanvasToImage(canvas)
+          return str
         }
         // 随机线
-        function drawline(canvas, context) {
+        function drawline (canvas, context) {
           context.moveTo(
             Math.floor(Math.random() * canvas.width),
             Math.floor(Math.random() * canvas.height)
-          ); // 随机线的起点x坐标是画布x坐标0位置，y坐标是画布高度的随机数
+          ) // 随机线的起点x坐标是画布x坐标0位置，y坐标是画布高度的随机数
           context.lineTo(
             Math.floor(Math.random() * canvas.width),
             Math.floor(Math.random() * canvas.height)
-          ); // 随机线的终点x坐标是画布宽度，y坐标是画布高度的随机数
-          context.lineWidth = 0.5; // 随机线宽
-          context.strokeStyle = "rgba(50,50,50,0.3)"; // 随机线描边属性
-          context.stroke(); // 描边，即起点描到终点
+          ) // 随机线的终点x坐标是画布宽度，y坐标是画布高度的随机数
+          context.lineWidth = 0.5 // 随机线宽
+          context.strokeStyle = 'rgba(50,50,50,0.3)' // 随机线描边属性
+          context.stroke() // 描边，即起点描到终点
         }
         // 随机点(所谓画点其实就是画1px像素的线，方法不再赘述)
-        function drawDot(canvas, context) {
-          var px = Math.floor(Math.random() * canvas.width);
-          var py = Math.floor(Math.random() * canvas.height);
-          context.moveTo(px, py);
-          context.lineTo(px + 1, py + 1);
-          context.lineWidth = 0.2;
-          context.stroke();
+        function drawDot (canvas, context) {
+          var px = Math.floor(Math.random() * canvas.width)
+          var py = Math.floor(Math.random() * canvas.height)
+          context.moveTo(px, py)
+          context.lineTo(px + 1, py + 1)
+          context.lineWidth = 0.2
+          context.stroke()
         }
         // 绘制图片
-        function convertCanvasToImage(canvas) {
-          document.getElementById("verifyCanvas").style.display = "none";
-          var image = document.getElementById("code_img");
-          image.src = canvas.toDataURL("image/png");
-          return image;
+        function convertCanvasToImage (canvas) {
+          document.getElementById('verifyCanvas').style.display = 'none'
+          var image = document.getElementById('code_img')
+          image.src = canvas.toDataURL('image/png')
+          return image
         }
         // 点击图片刷新
-        document.getElementById("code_img").onclick = function () {
-          resetCode();
-        };
-        function resetCode() {
-          $("#verifyCanvas").remove();
-          $("#code_img").before(
-            '<canvas width="100" height="40" id="verifyCanvas"></canvas>'
-          );
-          that.verVal = drawCode();
+        document.getElementById('code_img').onclick = function () {
+          resetCode()
         }
-      },
-    };
-    this.verificationCode.init();
+        function resetCode () {
+          $('#verifyCanvas').remove()
+          $('#code_img').before(
+            '<canvas width="100" height="40" id="verifyCanvas"></canvas>'
+          )
+          that.verVal = drawCode()
+        }
+      }
+    }
+    this.verificationCode.init()
     // console.log("vue已经被挂载出来了");
   },
   methods: {
-    ...mapActions(["getUserInfo"]),
-    onSubmit() {
-      this.login_btn();
+    ...mapActions(['getUserInfo']),
+    onSubmit () {
+      this.login_btn()
     },
-    login_btn() {
-      this.login_spin = true;
-      Spin.show();
+    login_btn () {
+      this.login_spin = true
+      Spin.show()
       this.$Message.loading({
-        content: "正在验证信息...",
-        duration: 0,
-      });
+        content: '正在验证信息...',
+        duration: 0
+      })
       if (
-        document.querySelector("#user_name").value == "" ||
-        document.querySelector("#user_password").value == ""
+        document.querySelector('#user_name').value == '' ||
+        document.querySelector('#user_password').value == ''
       ) {
         // 账号密码未输入
-        this.$Message.destroy();
-        this.$Message.warning("账号或密码未输入！");
-        this.login_spin = false;
-        Spin.hide();
-      }
-      // else if (
-      //   $("#verVal")
-      //     .val()
-      //     .toUpperCase() != this.verVal
-      // ) {
-      //   this.$Message.destroy();
-      //   this.$Loading.error();
-      //   this.$Message.error("验证码错误！");
-      //   // 更新验证码
-      //   $("#code_img").click();
-      //   this.login_spin = false;
-      // }
-      else {
-        let userName = $("#user_name").val().trim();
-        let password = $("#user_password").val();
+        this.$Message.destroy()
+        this.$Message.warning('账号或密码未输入！')
+        this.login_spin = false
+        Spin.hide()
+      } else {
+        // else if (
+        //   $("#verVal")
+        //     .val()
+        //     .toUpperCase() != this.verVal
+        // ) {
+        //   this.$Message.destroy();
+        //   this.$Loading.error();
+        //   this.$Message.error("验证码错误！");
+        //   // 更新验证码
+        //   $("#code_img").click();
+        //   this.login_spin = false;
+        // }
+        let userName = $('#user_name')
+          .val()
+          .trim()
+        let password = $('#user_password').val()
         // 模拟接口延迟
         this.getUserInfo({ userName, password })
-          .then((res) => {
-            this.$Message.destroy();
+          .then(res => {
+            this.$Message.destroy()
             this.$Message.success({
-              content: "登录成功！",
+              content: '登录成功！',
               background: true,
-              duration: 3,
-            });
+              duration: 3
+            })
             this.$router.push({
-              name: this.$config.homeName,
-            });
-            this.login_spin = false;
+              name: this.$config.homeName
+            })
+            this.login_spin = false
           })
-          .catch((res) => {
-            this.$Message.destroy();
-            Spin.hide();
-            this.login_spin = false;
-            this.$Message["error"]({
+          .catch(res => {
+            this.$Message.destroy()
+            Spin.hide()
+            this.login_spin = false
+            this.$Message['error']({
               background: true,
-              content: "登录超时，请联系管理员！",
-              duration: 3,
-            });
-          });
+              content: '登录超时，请联系管理员！',
+              duration: 3
+            })
+          })
       }
-    },
-  },
-};
+    }
+  }
+}
 </script>
