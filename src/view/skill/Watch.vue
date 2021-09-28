95ab42aa16e08813e9a90686e6a33486d9db80d9
YunDan1828361686 <1828361686@qq.com>
2021-09-28 16:02:53 +0800

    展开收起技术详解

--- src/view/skill/Watch.vue
+++ src/view/skill/Watch.vue
@@ -14,7 +14,7 @@ export default {
   name: "Watch",
   data() {
     return {
-      Input_value_1: "",
+      Input_value_1: ""
     };
   },
   methods: {},
@@ -31,12 +31,41 @@ export default {
       // 加上之后第一次定义时就会执行
       immediate: true,
       // 深度监听即当要监听对象或数组的时候需要添加deep:true属性
-      deep: true,
-    },
+      deep: true
+    }
   },
   computed: {},
-  mounted() {},
+  mounted() {
+    // 展开收起逻辑详解
+    [
+      {
+        title: "标题",
+        list: [{ title: "子级标题1" }, { title: "子级标题2" }]
+      }
+    ];
+    // H5循环数组渲染出所有行
+    // 在H5中子级添加ref，循环数组，给每行的ref添加原高度（即展开的高度），和一行的高度（即收起的高度）
+    // 重新渲染添加高度后的数组
+    // 重新刷新组件
+    this.dataSearch = res.data.data.standList;
+    this.dataSearch.map(item => {
+      item.isOpen = false;
+    });
+    this.$nextTick(() => {
+      const _dom = this.$refs.wrap;
+      let _list = this.dataSearch;
+      // 循环源数据，给每行数据添加原高度
+      for (let i = 0; i < this.dataSearch.length; i++) {
+        _list[i].openH = `${_dom[i].clientHeight}px`;
+        _list[i].closeH = "25px";
+      }
+      // 重新渲染
+      this.dataSearch = _list;
+      // 重新刷新组件
+      this.$forceUpdate();
+    });
+    // 渲染后查看没有展开的高度是多少，判断是否等于这个高度判断是否展示（展开收起按钮）
+  }
 };
 </script>
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
